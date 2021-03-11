85dd7cae8cf756203baff455eb574cfbcf66986e
jinseo-James-Cha <jinseo_cha@hotmail.com>
2021-03-11 10:12:57 +0900

    2021-03-11 search_linearSearching

---
+++ search/src/searching/LinearSearch.java
@@ -0,0 +1,25 @@
+package searching;
+
+
+public class LinearSearch {
+
+	public static void main(String[] args) {
+
+		/*
+		 * Linear Search(Sequential Search)
+		 * 
+		 * 
+		 * 종료조건
+		 * 1. 검색할 값을발견하지 못하고 배열의 끝을 지나간 경우
+		 * 2. 검색할 값과 같은 요소를 발견한 경우 
+		 * 
+		 * 보초법(Sentinel Method)
+		 * - 검색하려는 값을 배열 맨끝에 보초를 세운다.
+		 * 
+		 */
+		
+		int[] intArray = new int[7];
+		
+	}
+
+}
