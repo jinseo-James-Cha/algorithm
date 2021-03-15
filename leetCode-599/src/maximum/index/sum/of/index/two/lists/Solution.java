25d31cc77a0715650acd4051b224136aa2325bab
jinseo-James-Cha <jinseo_cha@hotmail.com>
2021-03-16 00:06:03 +0900

    2021-03-16 ver1 completed need refactoring

--- leetCode-599/src/maximum/index/sum/of/index/two/lists/Solution.java
+++ leetCode-599/src/maximum/index/sum/of/index/two/lists/Solution.java
@@ -8,37 +8,40 @@ public class Solution {
 
 		Solution s = new Solution();
 		
-		String[] list1 = {"Shogun","Tapioca Express","Burger King","KFC"};
-		String[] list2 = {"Piatti","The Grill at Torrey Pines","Hungry Hunter Steakhouse","Shogun"};
-		// output : Shogun
-		
-		String[] list3 = new String[] {"Shogun","Tapioca Express","Burger King","KFC"};
-		String[] list4 = new String[] {"KFC","Shogun","Burger King"};
-		// output : Shogun / sum index : 0+1
-		
-		String[] list5 = new String[] {"Shogun","Tapioca Express","Burger King","KFC"};
-		String[] list6 = new String[] {"KFC","Burger King","Tapioca Express","Shogun", "hollys" , "starbucks"};
-		// output : "KFC","Burger King","Tapioca Express","Shogun" 
-		// sum index : 3+0, 2+1, 1+2, 0+3
-		// arraylist index priority by first list index if sum is equal
-		
-		String[] list7 = new String[] {"KFC","Burger King","Tapioca Express","Shogun"};
-		String[] list8 = new String[] {"KNN","KFC","Burger King","Tapioca Express","Shogun"};
-		// output : "KFC","Burger King","Tapioca Express","Shogun"
-		// sum index : 0+1, 1+2, 2+3, 3+ 4
-		// less sum index, early in the output 
-		
-		String[] list9 = new String[] {"KFC"};
-		String[] list10 = new String[] {"KFC"};
+//		String[] list1 = {"Shogun","Tapioca Express","Burger King","KFC"};
+//		String[] list2 = {"Piatti","The Grill at Torrey Pines","Hungry Hunter Steakhouse","Shogun"};
+//		// output : Shogun
+//		
+//		String[] list3 = new String[] {"Shogun","Tapioca Express","Burger King","KFC"};
+//		String[] list4 = new String[] {"KFC","Shogun","Burger King"};
+//		// output : Shogun / sum index : 0+1
+//		
+//		String[] list5 = new String[] {"Shogun","Tapioca Express","Burger King","KFC"};
+//		String[] list6 = new String[] {"KFC","Burger King","Tapioca Express","Shogun", "hollys" , "starbucks"};
+//		// output : "KFC","Burger King","Tapioca Express","Shogun" 
+//		// sum index : 3+0, 2+1, 1+2, 0+3
+//		// arraylist index priority by first list index if sum is equal
+//		
+//		String[] list7 = new String[] {"Shogun","Tapioca Express","Burger King","KFC"};
+//		String[] list8 = new String[] {"KNN","KFC","Burger King","Tapioca Express","Shogun"};
+//		// output : "KFC","Burger King","Tapioca Express","Shogun"
+//		// sum index : 3+1, 2+2, 1+3, 0+4
+//		// less sum index, early in the output 
+//		
+//		String[] list9 = new String[] {"KFC"};
+//		String[] list10 = new String[] {"KFC"};
 		// output : "KFC"
 		
-//		String[] list11 = new String[] {"KFC","Burger King","Tapioca Express","Shogun"};
-//		String[] list12 = new String[] {"KFC","Burger King","Tapioca Express","Shogun"};
-//		
+		String[] list11 = new String[] {"Shogun","Tapioca Express","Burger King","KFC"};
+		String[] list12 = new String[] {"KFC","Shogun","Burger King"};
+		//output : shogun
 
+		String[] list13 = new String[] {"k", "KFC"};
+		String[] list14 = new String[] {"k", "KFC"};
+		//output : k
 		
 		//case 1
-		for(String str : s.findRestaurant(list7, list8)) {
+		for(String str : s.findRestaurant(list13, list14)) {
 			System.out.println(str);
 		}
 	}
@@ -48,24 +51,45 @@ public class Solution {
 	 * need to compare sum index of those common restaurant searched
 	 * 
 	 * key points 
-	 * 1. if the sum is equal, sort by list1's index descending or list2's index ascending
-	 * 2. if the sum is not equal, sort by the index sum ascending
+	 * 1. if the sum is equal, sort by list2's index ascending
+	 * 2. if the sum is different, only print the minimum number of sum
 	 * */
 	
+	
+	// Ver.1 
+	// Runtime 96ms
+	// 39.5 MB
+	// need a code re-factoring
 	public String[] findRestaurant(String[] list1, String[] list2) {
 	        
 		//String[] result = new String[list1.length];
 		ArrayList<String> strArray = new ArrayList<>(list1.length);
 		
-		for(int i = 0; i < list1.length; i++) {
-			for(int j = 0; j < list2.length; j++ ) {
-				if(list1[i].equals(list2[j])){
-					strArray.add(list1[i]);
+		int sumOfIndex = 0;
+		boolean isEmpty = true;
+		
+		for(int i = 0; i < list2.length; i++) {
+			
+			for(int j = 0; j < list1.length; j++ ) {
+				
+				if(list2[i].equals(list1[j])){
+					
+					if( isEmpty) {
+						strArray.add(list2[i]);
+						isEmpty = false;
+						sumOfIndex = i+j;
+					}
+					else if(sumOfIndex == i+j) {
+						strArray.add(list2[i]);
+					}
+					else if(sumOfIndex > i+j) {
+						strArray.removeAll(strArray);
+						strArray.add(list2[i]);
+					}
 				}
 			}
 		}
 		
-		System.out.println(strArray);
 		return strArray.toArray(new String[strArray.size()]);
 	}
 	
