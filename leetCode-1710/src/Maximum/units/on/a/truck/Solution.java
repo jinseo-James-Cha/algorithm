143af476d3074c737d64059c9e6868dc4b284c55
jinseo-James-Cha <jinseo_cha@hotmail.com>
2021-03-18 17:00:49 +0900

    2021-03-18 Ver1 submitted

--- leetCode-1710/src/Maximum/units/on/a/truck/Solution.java
+++ leetCode-1710/src/Maximum/units/on/a/truck/Solution.java
@@ -1,7 +1,8 @@
 package Maximum.units.on.a.truck;
 
-import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Solution {
 
@@ -23,45 +24,91 @@ public class Solution {
 		// example33
 		// [[2,1],[4,4],[3,1],[4,1],[2,4],[3,4],[1,3],[4,3],[5,3],[5,3]]
 		// 13
+		// output 48
+		int[][] e3 = {{2,1},{4,4},{3,1},{4,1},{2,4},{3,4},{1,3},{4,3},{5,3},{5,3}};
+		int e33 = 13;
 		
 		System.out.println(s.maximumUnits(e2, e22));
 		
 	}
-
 	
+	// Ver.1
+	// Runtime : 14 ms, 34.77%
+	// Memory  : 39.1MB, 77.24%
 	public int maximumUnits(int[][] boxTypes, int truckSize) {
-        
-		ArrayList<Integer> units = new ArrayList<>();
 		
+		// HashMap : not supported Descending order by using reverseOrder() method
+//		HashMap<Integer, Integer> h = new HashMap<>();
+		
+		// TreeMap : supported Descending, ordered by key value;
+		TreeMap<Integer, Integer> t = new TreeMap<>(Collections.reverseOrder());
+		int result = 0;
 		
 		for(int i = 0; i < boxTypes.length; i++) {
 			
-			units.add(boxTypes[i][1]);
-		}
-		
-		Collections.sort(units, Collections.reverseOrder());
-		
-		//System.out.println(units);
-		//Integer max = Collections.max(list); // will use this in version2
-		
-		int result = 0;
-		int temp = 0;
-		
-		for(int i = 0; i < units.size(); i++) {
-				
-			for(int j = 0; j < boxTypes.length; j++) {
-					
-				if(units.get(i) == boxTypes[j][1]) {
-					temp = boxTypes[j][0];
-					
-					for(int k = temp; k > 0 && truckSize > 0; k--) {
-						result += boxTypes[j][1];
-						truckSize--;
-					}
-				}
+			if(t.containsKey(boxTypes[i][1])) {
+				t.put(boxTypes[i][1], t.get(boxTypes[i][1]) + boxTypes[i][0]);
 			}
-			
+			else {
+				t.put(boxTypes[i][1], boxTypes[i][0]);
+			}
 		}
+		
+		for(Map.Entry<Integer, Integer> entry : t.entrySet()) {
+			
+			
+			if( truckSize - entry.getValue() >= 0) {
+					
+				result += (entry.getKey() * entry.getValue());
+				truckSize -= entry.getValue();
+			
+			} else {
+			
+				result += (entry.getKey() * truckSize);
+				truckSize = 0;
+			}
+
+		}
+		
+		return result;
+	}
+	
+
+	
+//	public int maximumUnits(int[][] boxTypes, int truckSize) {
+//        
+//		ArrayList<Integer> units = new ArrayList<>();
+//		
+//		
+//		for(int i = 0; i < boxTypes.length; i++) {
+//			
+//			units.add(boxTypes[i][1]);
+//		}
+//		
+//		Collections.sort(units, Collections.reverseOrder());
+//		
+//		//System.out.println(units);
+//		//Integer max = Collections.max(list); // will use this in version2
+//		
+//		int result = 0;
+//		int temp = 0;
+//		
+//		for(int i = 0; i < units.size(); i++) {
+//				
+//			for(int j = 0; j < boxTypes.length; j++) {
+//					
+//				if(units.get(i) == boxTypes[j][1]) {
+//					temp = boxTypes[j][0];
+//					
+//					for(int k = temp; k > 0 && truckSize > 0; k--) {
+//						result += boxTypes[j][1];
+//						System.out.println(result);
+//						truckSize--;
+//					}
+//				}
+//			}
+//			
+//		}
 		
 		
 		
@@ -76,8 +123,8 @@ public class Solution {
 //			}
 //		}
 //		
-		return result;
-    }
+//		return result;
+//    }
 	
 	
 	
