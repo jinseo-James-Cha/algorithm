4e3cf3c10b36925ca6a0bc31ac0a9a85a5ed4d69
jinseo-James-Cha <jinseo_cha@hotmail.com>
2021-03-25 21:16:43 +0900

    2021-03-25 Ver.1 submitted

--- leetCode-1518/src/water/bottles/Solution.java
+++ leetCode-1518/src/water/bottles/Solution.java
@@ -22,29 +22,34 @@ public class Solution {
 		
 		Solution s = new Solution();
 		
-//		System.out.println(s.numWaterBottles(9, 3));
-		//output : 13
+		System.out.println(s.numWaterBottles(9, 3));
+//		output : 13
 		
 		System.out.println(s.numWaterBottles(15, 4));
 //		//output : 19
 //		
-//		System.out.println(s.numWaterBottles(5, 5));
+		System.out.println(s.numWaterBottles(5, 5));
 //		//output : 6
 //		
-//		System.out.println(s.numWaterBottles(2, 3));
+		System.out.println(s.numWaterBottles(2, 3));
 //		//output : 2
 		
 	}
 	
+	// Ver.1
+	// Runtime : 1 ms, 100%   
+	// Memory  : 35.9 MB, 36.61%
+	// Comment : need to improve on memory usage.
+	
 	public int numWaterBottles(int numBottles, int numExchange) {
         
 		int result = numBottles;
-		
 		while(numBottles >= numExchange) {
-			
+						
+			result += numBottles/numExchange ;
+			numBottles = (numBottles/numExchange) + (numBottles % numExchange);
 			
 		}
-		
 		
 		return result;
     }
